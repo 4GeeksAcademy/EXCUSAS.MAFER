004f3e2318d9c4c003a852377e581f0984387b5f
fer-orfao <133964332+fer-orfao@users.noreply.github.com>
2025-06-25 16:55:25 +0000

    Primer commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-import "bootstrap";
-import "./style.css";
+window.onload = function () {
 
+  let who = ['Mi perro', 'El unicornio del vecino', 'El alienígena de la esquina', 'Un pajarito'];
+  let action = ['himnotizó', 'se comió', 'desapareció con la mente', 'multiplicó'];
+  let what = ['a Papá Noel', 'una gallina que iba pasando', 'a unos peces', 'a pikachu'];
+  let when = ['durante la 2da guerra mundial','en 1994','mientras dormía','en la madrugada','mientras jugaba a la play'
+  ];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+ 
+  let excuse =
+    who[whoIndex] + ' ' +
+    action[actionIndex] + ' ' +
+    what[whatIndex] + ' ' +
+    when[whenIndex];
+
+  document.getElementById("excuse").innerHTML = excuse;
 };
